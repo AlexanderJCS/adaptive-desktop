318a17bdfdd971f9be8f15bbe2ceab0e1a454495
Alex <alexandercastronovo@gmail.com>
2022-05-21 18:37:18 -0400

    Added time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
+import datetime
 import logging
 import ctypes
 import time
@@ -20,42 +21,76 @@ VALID_FORMATS = ("png", "jpg", "jpeg", "gif")
 
 
 def get_weather_data(lat, lon):
+    # Gets the weather data using the openweathermap API
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={config.get('key')}"
 
     r = requests.get(url)
     return r.json()
 
 
-def set_desktop(absolute_path):
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, absolute_path, 0)
+def find_valid_filepath(weather, time_of_day):
+    # Find the valid filepath for the background
+    for file_format in VALID_FORMATS:
+        path = f"{config.get('backgrounds_path')}\\{time_of_day}_{weather}.{file_format}"
+
+        if os.path.isfile(path):
+            return path
+
+    logging.warning(f"No image found for the weather condition \"{weather}\" and the time of day \"{time_of_day}\"")
 
 
-def set_desktop_with_weather(weather_id):
+def set_desktop(time_of_day, weather_condition):
+    # Finds the file path for the background image and sets that image as the desktop background
+    file_path = find_valid_filepath(weather_condition, time_of_day)
+
+    if file_path is None:
+        return
+
+    # Sets the desktop background using the windll
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, file_path, 0)
+    logging.info(f"Set desktop background to {file_path}")
+
+
+def find_weather_name(weather_id):
+    # Find the weather name from the weather IDs
     for condition, value in IDS.items():
         if weather_id not in value:
             continue
 
-        for file_format in VALID_FORMATS:
-            path = f"{config.get('backgrounds_path')}\\{condition}.{file_format}"
-            if os.path.exists(path):
-                set_desktop(path)
-                break
+        return condition
 
-        else:
-            logging.warning(f"No background found for ID {weather_id}. To find a list of weather IDs, visit"
-                            f" https://openweathermap.org/weather-conditions")
+    logging.warning(f"No background found for ID {weather_id}. To find a list of weather IDs, visit"
+                    f" https://openweathermap.org/weather-conditions")
+
+
+def find_time_of_day():
+    # Find if the current time is between sunrise and sunset
+    now = datetime.datetime.now()
+    sunrise = now.replace(hour=config.get("sunrise_hour"), minute=config.get("sunrise_minute"))
+    sunset = now.replace(hour=config.get("sunset_hour"), minute=config.get("sunset_minute"))
+
+    if sunrise < now < sunset:
+        return "day"
+
+    return "night"
 
 
 def main():
     logging.info("Starting...")
 
     while True:
+        # Gather weather data
         logging.debug("Gathering weather data...")
         data = get_weather_data(config.get("lat"), config.get("lon"))
-
         weather_id = data["weather"][0]["id"]
         logging.debug(f"Collected weather data. ID: {weather_id}")
-        set_desktop_with_weather(weather_id)
+        condition = find_weather_name(weather_id)
+
+        # Find the time of day
+        time_of_day = find_time_of_day()
+
+        # Use this information to set the desktop background
+        set_desktop(time_of_day, condition)
         time.sleep(config.get("refresh_time"))
 
 
